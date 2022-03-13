e5d93140ce0db32ddd04d4fd625c1aaf31ded83e
alexis bustamante <alexisbustamantecisternas@gmail.com>
2022-03-12 21:00:13 -0300

    creo rutas de Login y Autenticación de usuario, JWT para la session, SEQUELIZE para la busqueda de usuario.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,5 @@ import { Router } from 'express';
 import auth from '../controllers/auth.controllers';
 
 const router = Router();
-router.post('/register', auth.register);
 router.post('/login', auth.login);
-// router.post('/signIn', auth.signIn);
-// router.post('/servicio', auth.servicio);
 module.exports = router;
