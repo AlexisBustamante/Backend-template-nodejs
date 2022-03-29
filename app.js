8f70933d10e8e93ff8b0be1c4caccefd5dda2732
alexis bustamante <alexisbustamantecisternas@gmail.com>
2022-03-29 16:45:02 -0300

    prueas heroku

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import app from './server/server';
 
 
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get("port")}`);
-    console.log(`http://localhost:${app.get("port")}`);
+   // console.log(`Server on port ${app.get("port")}`);
+    //console.log(`http://localhost:${app.get("port")}`);
 });
